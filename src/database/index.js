ba00a90a0c0bbf2a4fe85609ec9aab5a08e40064
devrdias <devrdias@gmail.com>
2019-10-15 22:01:27 -0400

    configure sequelize/postgres.  Add Student Model and Migration file

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+
+import Student from '../app/models/Student';
+
+const models = [Student];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
---
+++ src/database/migrations/20191016015504-create-students.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      birth: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      weight: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      height: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('students');
+  },
+};
